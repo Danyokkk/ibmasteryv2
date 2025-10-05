11628a71de73404f4b081b49dfeb19da78637746
YourGitHubUsername <your_email@example.com>
2025-10-05 12:12:35 +0300

    Fix build issue with additional polyfills

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,29 @@
 import { defineConfig } from 'vite';
 import nodePolyfills from 'rollup-plugin-node-polyfills';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 export default defineConfig({
   plugins: [
     nodePolyfills()
   ],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true
+        })
+      ]
+    }
+  },
   resolve: {
     alias: {
-      crypto: 'crypto-browserify'
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      buffer: 'buffer'
     }
   }
 });
