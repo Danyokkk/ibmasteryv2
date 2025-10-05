8ac1bb68f77eba51d814ba7b3904a87fce9e9297
YourGitHubUsername <your_email@example.com>
2025-10-05 12:46:02 +0300

    Fix: Configure project for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const isProduction = mode === 'production';
+
     return {
+      base: isProduction ? '/ibmasteryv2/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
